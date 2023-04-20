9ce650eb091dd69e1bd8a3c20132d5b564597e9e
iam6adman <iam6adman@outlook.com>
2023-04-20 21:48:29 +0300

    add example023

---
+++ Example023/Program.cs
@@ -0,0 +1,13 @@
+﻿// Функция ввода
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int number = ReadInt("Введите число");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
